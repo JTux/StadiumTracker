76442f27e688de113c3e1246a82bd7d4da061dbe
Joshua Tucker <thejltucker@gmail.com>
2018-08-15 08:57:29 -0400

    Add project files.

---
+++ StadiumTracker.WebMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace StadiumTracker.WebMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ StadiumTracker.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StadiumTracker.WebMVC.MvcApplication" Language="C#" %>
